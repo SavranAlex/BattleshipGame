7fa49f1988fa1f890a00ecee6b6d26c6f6e11ca1
Alex <Savranalex@gmail.com>
2018-09-26 19:59:59 +1000

    Pair class for hashmap value

---
+++ src/player/Pair.java
@@ -0,0 +1,19 @@
+package player;
+
+public class Pair {
+    int col;
+    int raw;
+
+    Pair(int col, int raw) {
+        this.col = col;
+        this.raw = raw;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRaw() {
+        return raw;
+    }
+}
