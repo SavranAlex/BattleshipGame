ef0e5cda55b683088264e9577ee10ff10f8461ea
Alex <Savranalex@gmail.com>
2018-10-08 15:13:25 +1100

    RandomGuessPlayer works, but choosing random key works strange

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -1,52 +1,35 @@
 package player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeMap;
 
-import ship.Ship;
 import world.World;
 import world.World.Coordinate;
-import world.World.ShipLocation;
-
-
 /**
  * Random guess player (task A).
  * Please implement this class.
  *
  * @author Youhan Xia, Jeffrey Chan
  */
-public class RandomGuessPlayer implements Player{
-
+public class RandomGuessPlayer implements Player {
     private int col;
     private int raw;
     private ArrayList<World.ShipLocation> shipsLocations;
-    private ArrayList<ShipLocation> remainingShips;
+    private ArrayList<World.ShipLocation> remainingShips;
     //private ArrayList<World.Coordinate> shots;
     //private HashMap<Integer, TargetedCell> shots;
     private TreeMap<Integer, TargetedCell> shots;
     private int k;
-    private Coordinate coor;
-    private ShipLocation targetedShipLocation;
+    //private World.Coordinate cr;
+    private Coordinate cr;
+    //private World.ShipLocation targetedShipLocation;
     //private Answer answer;
     //private Guess guess;
     private  ArrayList<Coordinate> hittedCoors;
     private boolean shootStatus;
     //private int count;
-    private ShipLocation removingShip;
-    private boolean end;
-
-
-//    public TargetedCell pickShotCell() {
-//        Random rnd = new Random();
-//        TargetedCell shootCell = shots.get(rnd.nextInt(k));
-//        if (shootCell.shootStatus) {
-//            return pickShotCell();
-//        }
-//        return shootCell;
-//    }
-
-
-    public RandomGuessPlayer() {
-    }
+    private World.ShipLocation removingShip;
 
     @Override
     public void initialisePlayer(World world) {
@@ -67,36 +50,32 @@ public class RandomGuessPlayer implements Player{
                 k++;
             }
         }
-        //count = k;
-        //System.out.println("map size: " + shots.size() + " ks " + k + " count " + count);
-
     } // end of initialisePlayer()
 
     @Override
     public Answer getAnswer(Guess guess) {
-        //System.out.println(guess.toString());
         Answer answer = new Answer();
-        coor = new Coordinate();
-        coor.column = guess.column;
-        coor.row = guess.row;
+        cr = new Coordinate();
+        cr.column = guess.column;
+        cr.row = guess.row;
         answer.isHit = false;
-        //targetedShipLocation.coordinates.add(coor);
-      for (ShipLocation possibleShip : remainingShips) {
-          if (possibleShip.coordinates.contains(coor)) {
-              answer.isHit = true;
-              //hittedCoors.add(coor);
-              possibleShip.coordinates.remove(coor);
-          }
-          if (possibleShip.coordinates.isEmpty()) {
-          //if (hittedCoors.containsAll(possibleShip.coordinates)) {
-              answer.shipSunk = possibleShip.ship;
-              //remainingShips.remove(possibleShip);
-              removingShip = possibleShip;
-              //System.out.println("shots size " + shots.size() + " ship name: " + possibleShip.ship.name() + " removed? " + remainingShips.remove(possibleShip));
-              System.out.println("ship destroyed" + removingShip.ship.name());
-              System.out.println("counts: " + k + " ships remained: " + remainingShips.size());
-          }
-      }
+        //targetedShipLocation.coordinates.add(cr);
+        for (World.ShipLocation possibleShip : remainingShips) {
+            if (possibleShip.coordinates.contains(cr)) {
+                answer.isHit = true;
+                //hittedCoors.add(cr);
+                possibleShip.coordinates.remove(cr);
+            }
+            if (possibleShip.coordinates.isEmpty()) {
+                //if (hittedCoors.containsAll(possibleShip.coordinates)) {
+                answer.shipSunk = possibleShip.ship;
+                //remainingShips.remove(possibleShip);
+                removingShip = possibleShip;
+                //System.out.println("shots size " + shots.size() + " ship name: " + possibleShip.ship.name() + " removed? " + remainingShips.remove(possibleShip));
+                System.out.println("ship destroyed" + removingShip.ship.name());
+                System.out.println("counts: " + k + " ships remained: " + remainingShips.size());
+            }
+        }
         remainingShips.remove(removingShip);
         return answer;
     } // end of getAnswer()
@@ -128,8 +107,6 @@ public class RandomGuessPlayer implements Player{
 
     @Override
     public void update(Guess guess, Answer answer) {
-//        guess = this.makeGuess();
-//        answer = this.getAnswer();
         System.out.println("update method " + remainingShips.size());
     } // end of update()
 
